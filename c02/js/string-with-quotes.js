5b40a241802fd5de3fb248ebd31a1cafe6f9c2f1
unknown <jon.collins71@gmail.com>
2016-08-08 16:38:25 +0300

    page 65 complete

--- c02/js/string-with-quotes.js
+++ c02/js/string-with-quotes.js
@@ -1,1 +1,11 @@
-// Create variables to hold the title and note text.var title; var message;// Assign values to these variables.title = "Molly's Special Offers";message = '<a href=\"sale.html\">25% off!</a>';// Get the element with an id of title.var elTitle = document.getElementById('title');// Replace the content of this element.elTitle.textContent = title;// Get the element with an id of note.var elNote = document.getElementById('note');// Replace the content of this element.elNote.innerHTML = message;/* NOTE: textContent does not work in IE8 or earlierYou can use innerHTML on line 12, but note the security issues on p228-231elTitle.innerHTML = title;innerHTML is used on line 17 because it is adding markup (not just text)*/
+
+var title;	// creates new variable called title
+var message; // creates new variable called message
+title = "Marta`s Special Offers"; // assigns value to title
+message = "<a href=\"sale.html\">25% off!</a>"; // assigns value to mesaage (using \to escape quotation marks so they become part of the value)
+
+var elTitle = document.getElementById("title"); // get element to change
+elTitle.innerHTML = title; // use innerHTML to put value in element
+var elNote = document.getElementById("note"); // get element to change
+elNote.innerHTML = message; // use innerHTML to put value in element
+
